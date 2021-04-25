f89823330872396e1d10be7e323d1ba80f5b17e2
camila monteiro <monteiro.camis@gmail.com>
2021-04-25 01:07:25 -0300

    :sparkles: add absolute, relative, flex layout, scroll e frame

---
+++ AppGallery/AppGallery/AppGallery/xamarinForms/Layouts/BarraDeRolagem/BarraDeRolagem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppGallery.xamarinForms.Layouts.BarraDeRolagem
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BarraDeRolagem : ContentPage
+    {
+        public BarraDeRolagem()
+        {
+            InitializeComponent();
+        }
+
+        private void RolarPara(object sender, EventArgs e)
+        {
+            BarraDeScroll.ScrollToAsync(LabelEscolhida, ScrollToPosition.Start, true);
+        }
+        
+
+        /*BarraDeRolagem.ScrollToAsync(0,100,true)*/
+    }
+}
